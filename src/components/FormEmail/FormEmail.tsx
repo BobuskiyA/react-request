9da1801aaadd9627b055608631e061a0447cea67
Bobuskiy <bobuskiy.olexander@example.com>
2024-01-27 15:33:36 +0100

    Fix

--- src/components/FormEmail/FormEmail.tsx
+++ src/components/FormEmail/FormEmail.tsx
@@ -1,6 +1,5 @@
 import React, { useState, FormEvent, ChangeEvent } from "react";
 import "./FormEmail.css";
-import "../../email/sender.php";
 function FormEmail() {
   return <div className="formsent"></div>;
 }
